9347b27835f8dde5e3710ae480b87a82a5daba99
Roborhugo <robothugoand@gmail.com>
2022-05-09 16:04:17 +0200

    Finished script for tesript for testing

--- R/script.R
+++ R/script.R
@@ -31,8 +31,8 @@ parseFile <- function(path) {
 	dataframe <- read.csv(path, F)
 	names(dataframe) <- "time"
 
-	indices <- 1:length(dataframe$time)
-	dataframe$index <- indices
+	iterations <- 1:length(dataframe$time)
+	dataframe$iteration <- iterations
 	
 	dataframe[, c(2,1)]
 }
@@ -53,7 +53,7 @@ plotData <- function(data, name) {
 system("java -classpath ../Java/ -Xint MergeSort ../Scala/random.txt ../Java/resultXint.txt 600")
 
 # JIT Java
-system("java -classpath ../Java/ MergeSort ../Scala/random.txt result.txt 600")
+system("java -classpath ../Java/ MergeSort ../Scala/random.txt result.txt 1200")
 
 # AOT Java
 system("java -XX:+UnlockExperimentalVMOptions -XX:AOTLibrary=../Java/MergeSort.so -classpath ../Java MergeSort ../Scala/random.txt ../Java/result.txt 600")
@@ -77,7 +77,7 @@ plotData(parseFile("../C++/result.txt"), "C++.pdf")
 
 
 # Get average time of one running, iterations from start to stop. (make sure result path is R/result.txt i.e. just result.txt)
-getAverage <- function(runCommand, start, stop) {
+getMean <- function(runCommand, start, stop) {
 	system(runCommand)
 	dataframe <- parseFile("result.txt")
 	data <- dataframe$time
@@ -85,26 +85,57 @@ getAverage <- function(runCommand, start, stop) {
 }
 
 # Test
-#getAverage("java -classpath ../Java/ MergeSort ../Scala/random.txt result.txt 600", 450, 600)
+#getMean("java -classpath ../Java/ MergeSort ../Scala/random.txt result.txt 600", 450, 600)
 
 
 # Get a list of average times. See getAverage() for more info.
-getAverageList <- function(runCommand, start, stop, n) {
+getMeanList <- function(runCommand, start, stop, n) {
 	list <- c()
 	for (i in 1:n) {
-		list <- c(list, getAverage(runCommand, start, stop))
+		list <- c(list, getMean(runCommand, start, stop))
 	}
 	
 	list
 }
 
 # Test 
-#getAverageList("java -classpath ../Java/ MergeSort ../Scala/random.txt result.txt 600", 450, 600, 10)
+#getMeanList("java -classpath ../Java/ MergeSort ../Scala/random.txt result.txt 600", 450, 600, 10)
 
 
+# Mean lists for every program
+javaJIT <- getMeanList("java -classpath ../Java/ MergeSort ../Scala/random.txt result.txt 600", 450, 600, 10)
+
+javaXint <- getMeanList("java -classpath ../Java/ -Xint MergeSort ../Scala/random.txt ../Java/resultXint.txt 600", 1, 600, 10)
+
+javaAOT <- getMeanList("java -XX:+UnlockExperimentalVMOptions -XX:AOTLibrary=../Java/MergeSort.so -classpath ../Java MergeSort ../Scala/random.txt ../Java/result.txt 600", 1, 600, 10)
+
+cpp <- getMeanList("../C++/MergeSort ../Scala/random.txt ../C++/result.txt 600", 1, 600, 10)
 
 
+# Print mean of mean list plus mean list
+print("Java JIT:")
+mean(javaJIT)
+javaJIT
+
+print("Java Xint:")
+mean(javaXint)
+javaXint
+
+print("Java AOT:")
+mean(javaAOT)
+javaAOT
+
+print("C++:")
+mean(cpp)
+cpp
 
 
+# t-tests
+print("JIT & Xint")
+t.test(javaJIT, javaXint)
 
+print("JIT & AOT")
+t.test(javaJIT, javaAOT)
 
+print("JIT & C++")
+t.test(javaJit, cpp)
